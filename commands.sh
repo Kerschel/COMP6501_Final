b29bf9211be99d31688082f85560c38a60ee3dc2
Andrew Singh <princeandrew01@gmail.com>
2018-11-24 15:42:15 -0400

    Update commands.sh
    Added comments.

--- commands.sh
+++ commands.sh
@@ -1,7 +1,11 @@
+#Creates an amazon cloud formation stack, replace the your_name with something unique
 aws cloudformation create-stack --stack-name ec2stack-your_name --region us-east-1 --template-body file://$PWD/deployment.yml
 
+#Uses docker compose to run the app.yml to create the necessary containers
 docker-compose -H tcp://EC2_Instance_IPv4:2375 -f app.yml up -d
 
+#Takes down the containers that were created via the app.yml
 docker-compose -H tcp://EC2_Instance_IPv4:2375 -f app.yml down
 
+#Deletes the amazon cloud formation stack
 aws cloudformation create-stack --delete-name ec2stack-EC2_Instance_IPv4 --region us-east-1 
